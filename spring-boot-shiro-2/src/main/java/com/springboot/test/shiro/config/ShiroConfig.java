8fc1bafd25c480e57a2013fc77ccc9d19205f822
wangsaichao <wangsaichao@ruubypay.com>
2018-05-14 00:19:57 +0800

    springboot整合shiro-Whitelabel Error Page解决

--- spring-boot-shiro-2/src/main/java/com/springboot/test/shiro/config/ShiroConfig.java
+++ spring-boot-shiro-2/src/main/java/com/springboot/test/shiro/config/ShiroConfig.java
@@ -181,8 +181,8 @@ public class ShiroConfig {
             public void customize(ConfigurableEmbeddedServletContainer container) {
 
                 ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthorized.html");
-                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/unauthorized.html");
-                ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/unauthorized.html");
+                ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+                ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
 
                 container.addErrorPages(error401Page, error404Page, error500Page);
             }
